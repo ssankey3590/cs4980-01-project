a31e13c75121842f87efa7304519e6c80961f3f3
Stewart Sankey <ssankey@stewartsankey.com>
2024-11-22 12:12:08 -0600

    added IMO 2023 problems

--- Proofs/IMO2023_Problem2.thy
+++ Proofs/IMO2023_Problem2.thy
@@ -1,16 +1,52 @@
 theory IMO2023_Problem2
-imports Complex_Main
+  imports Main Complex_Geometry
+
 begin
 
-(* Define the function type we're looking for *)
-typedef pos_real = "{x::real. x > 0}"
-  morphisms real_of_pos pos_of_real
-  by (simp add: linordered_field_no_ub)
+(* First, let's define our basic geometric objects *)
+locale triangle_configuration =
+  fixes A B C :: complex    (* Points as complex numbers *)
+  fixes S E D L P :: complex
+  assumes acute: "is_acute_triangle A B C"
+  and AB_less_AC: "dist A B < dist A C"
+  and omega_circle: "on_circle B D L"    (* Points B,D,L lie on circle \<omega> *)
+  and Omega_circle: "on_circle A B C"    (* Points A,B,C lie on circle \<Omega> *)
+  and S_midpoint: "S = midpoint_arc C B A"
+  and D_perpendicular: "is_perpendicular (vector A D) (vector B C)"
+  and E_on_AD: "collinear A D E"
+  and E_on_Omega: "on_circle A B C E"
+  and L_parallel: "is_parallel (vector D L) (vector B C)"
+  and L_on_BE: "collinear B E L"
+  and P_intersection: "P \<noteq> B \<and> on_circle B D L P \<and> on_circle A B C P"
 
-(* Main theorem statement *)
-theorem imo2023_p2:
-  fixes f :: "pos_real \<Rightarrow> pos_real"
-  assumes "\<forall>x y. real_of_pos (f x) * real_of_pos (f y) = 
-          real_of_pos (f (pos_of_real (real_of_pos x * real_of_pos (f y))))"
-  shows "f = (\<lambda>x. pos_of_real 1) \<or> f = (\<lambda>x. x)"
+(* Now let's state our main theorem *)
+theorem tangent_meets_bisector:
+  assumes config: "triangle_configuration A B C S E D L P"
+  shows "let t = tangent_line \<omega> P;
+         b = angle_bisector B A C
+         in intersect t (line B S) \<in> b"
   sorry
+(* We'll break this into lemmas *)
+
+lemma power_of_point:
+  assumes "triangle_configuration A B C S E D L P"
+  shows "power_of_point P \<omega> = power_of_point P \<Omega>"
+  sorry
+
+lemma angle_properties:
+  assumes "triangle_configuration A B C S E D L P"
+  shows "angle B P S = angle B A C / 2"
+
+(* Main proof *)
+proof -
+  (* The proof would involve several steps:
+     1. Use power of point theorem for P with respect to both circles
+     2. Utilize the parallel property of DL to BC
+     3. Apply angle relationships in cyclic quadrilaterals
+     4. Use properties of angle bisectors
+     5. Combine these to show the tangent line meets BS on the angle bisector
+  *)
+  show ?thesis 
+  sorry
+qed
+end
--- Proofs/IMO2023_Problem4.thy
+++
@@ -1,9 +0,0 @@
-theory IMO2023_Problem4
-imports Complex_Main
-begin
-
-(* Main theorem statement *)
-theorem imo2023_p4:
-  fixes f :: "real ⇒ real"
-  assumes "∀x y. f(x + y + f(x*y)) = f(x) * f(y)"
-  shows "f = (λx. 0) ∨ f = (λx. 1)"
