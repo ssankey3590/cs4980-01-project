ed5b6a2125e2512e7378258e2c7fd4e4acdaa397
Stewart Sankey <stewart-sankey@uiowa.edu>
2024-11-29 01:13:33 -0600

    updated Problem 1

--- Proofs/IMO2023_Problem1.thy
+++ Proofs/IMO2023_Problem1.thy
@@ -1,5 +1,5 @@
 theory IMO2023_Problem1
-imports Main "HOL-Computational_Algebra.Factorial_Ring"
+imports Main "HOL-Number_Theory.Number_Theory"
 
 
 (*
@@ -11,30 +11,73 @@ then d_i divides d_(i+1) + d_(i+2) for every 1 \<le> i \<le> k-2.
 
 
 *)
+
 begin
 
+(* First, let's define what it means for a number to be a divisor *)
+definition is_divisor :: "nat \<Rightarrow> nat \<Rightarrow> bool" where
+"is_divisor d n \<equiv> d dvd n"
 
-fun is_composite :: "int \<Rightarrow> bool" where
-  "is_composite n = (\<exists>a b. 1 < a \<and> a < n \<and> 1 < b \<and> b < n \<and> a * b = n)"
+(* Define a function to get all divisors of a number *)
+definition divisors :: "nat \<Rightarrow> nat set" where
+"divisors n = {d. d dvd n}"
 
-fun divisors :: "int \<Rightarrow> int list" where
-  "divisors n = [d. d \<leftarrow> [1..n], n mod d = 0]"
+(* Define our property for a list of divisors *)
+definition has_divisor_property :: "nat \<Rightarrow> bool" where
+"has_divisor_property n \<equiv> 
+  let divs = sorted_list_of_set (divisors n)
+  in length divs \<ge> 3 \<and>
+     (\<forall>i < length divs - 2. 
+      divs!i dvd (divs!(i+1) + divs!(i+2)))"
 
-fun satisfies_condition :: "int \<Rightarrow> bool" where
-  "satisfies_condition n =
-    (let divs = divisors n
-    in \<forall>i. 1 \<le> i \<and> i \<le> length divs - 2 \<longrightarrow> divs!i dvd (divs!(i+1) + divs!(i+2)))"
-
-fun find_valid_numbers :: "int \<Rightarrow> int list" where
-  "find_valid_numbers limit = [n. n \<leftarrow> [1..limit], is_composite n \<and> satisfies_condition n]"
-
-lemma solution_02:
-  fixes n i k::"int"
-  assumes "n \<ge> 1"
-  assumes "prime p"
-  assumes "r \<ge> 2"
-  assumes "\<forall>n. n \<in> set (find_valid_numbers limit)" 
-  shows "n = p^r"
+(* Helper lemma: For prime powers p^r with r \<ge> 2, the divisors are exactly 1,p,p^2,...,p^r *)
+lemma prime_power_divisors:
+  assumes "prime p" "r \<ge> 2"
+  shows "divisors (p^r) = {p^i |i. i \<le> r}"
+  using assms
   sorry
 
+(* Main theorem part 1: Prime powers satisfy the property *)
+theorem prime_power_has_property:
+  assumes "prime p" "r \<ge> 2"
+  shows "has_divisor_property (p^r)"
+proof -
+  let ?divs = "sorted_list_of_set (divisors (p^r))"
+  have div_form: "?divs = [1, p, p^2, ..., p^r]"
+    using assms prime_power_divisors
+    sorry
+  
+  (* For any i < length divs - 2, p^i divides p^(i+1) + p^(i+2) *)
+  have "\<forall>i < length ?divs - 2. ?divs!i dvd (?divs!(i+1) + ?divs!(i+2))"
+    using assms
+    sorry
+  
+  thus ?thesis
+    using assms
+    sorry
+qed
+
+(* Main theorem part 2: Only prime powers satisfy the property *)
+theorem only_prime_powers_have_property:
+  assumes "has_divisor_property n" "n > 1"
+  shows "\<exists>p r. prime p \<and> r \<ge> 2 \<and> n = p^r"
+proof -
+  let ?divs = "sorted_list_of_set (divisors n)"
+  
+  (* If n is not a prime power, it has at least two prime factors *)
+  have "\<exists>p r. prime p \<and> r \<ge> 2 \<and> n = p^r"
+    using assms
+    sorry
+  
+  thus ?thesis
+    by auto
+qed
+
+(* Final combined theorem *)
+theorem main_characterization:
+  "n > 1 \<and> has_divisor_property n \<longleftrightarrow> (\<exists>p r. prime p \<and> r \<ge> 2 \<and> n = p^r)"
+  using prime_power_has_property only_prime_powers_have_property
+  by (metis not_numeral_le_zero one_le_power order_less_le prime_ge_1_nat prime_power_eq_one_iff)
+
 end
+
